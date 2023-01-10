655b92f5ee813c42f4b6938c0177a19f0b07beed
Naomi Timothy <naomitim17.com>
2023-01-10 11:03:47 +0300

    added edit profile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import EditProfile from "./pages/EditProfile";
 
-import Home from './pages/Home'
-import Login from './pages/login'
-import ResetPassword from './pages/ResetPassword'
-import Start from './pages/Start'
+import Home from "./pages/Home";
+import Login from "./pages/login";
+import ResetPassword from "./pages/ResetPassword";
+import Start from "./pages/Start";
 
 export const routes = [
-  { path: '/', element: <Start /> },
-  { path: '/home', element: <Home /> },
-  { path: '/reset-password', element: <ResetPassword /> },
-  { path: '/login', element: <Login /> },
-]
+	{ path: "/", element: <Start /> },
+	{ path: "/home", element: <Home /> },
+	{ path: "/reset-password", element: <ResetPassword /> },
+	{ path: "/login", element: <Login /> },
+	{ path: "/edit-profile", element: <EditProfile /> },
+];
 
 function App() {
-  return <RouterProvider router={createBrowserRouter(routes)} />
+	return <RouterProvider router={createBrowserRouter(routes)} />;
 }
 
-export default App
+export default App;
