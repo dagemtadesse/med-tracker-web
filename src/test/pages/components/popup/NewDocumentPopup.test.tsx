80c655834e8b750f3af319fcf139943bcf9c497f
Dagem Tadesse <dagem.seyfu@gmail.com>
2023-02-13 08:23:43 +0300

    update test cases

--- src/test/pages/components/popup/NewDocumentPopup.test.tsx
+++ src/test/pages/components/popup/NewDocumentPopup.test.tsx
@@ -1,11 +1,17 @@
 import { render } from "@testing-library/react";
 import NewDocumentPopup from "../../../../components/popup/NewDocumentPopup";
-import { DocumentContextProvider } from "../../../../contexts/DocumentContext";
+import DocumentContext, { DocumentContextProvider } from "../../../../contexts/DocumentContext";
+import { useContext } from "react";
 
+const Wrapper = () => {
+  const docContext = useContext(DocumentContext)
+
+  return docContext ? <NewDocumentPopup /> : <div></div>
+}
 it("should render correctly", () => {
   render(
     <DocumentContextProvider>
-      <NewDocumentPopup />
+      
     </DocumentContextProvider>
   );
 });
--- src/test/pages/components/profile/ViewProfile.test.tsx
+++ src/test/pages/components/profile/ViewProfile.test.tsx
@@ -1,12 +1,11 @@
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import ViewProfile from "../../../../components/profile/ViewProfile";
-import { user } from "../../../../contexts/dummy-data";
 
 it("should render correctly", () => {
   render(
     <MemoryRouter>
-      <ViewProfile close={() => {}} user={user} />
+      <ViewProfile close={() => {}}  />
     </MemoryRouter>
   );
 });
